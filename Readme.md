bab0ca36387c40399c683be515646a2a6a69b663
Simon Struck <simon@simonscode.org>
2022-08-25 14:44:08 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# ST25DV Decoder for Sigrok and PulseView
+
+![](demo.png)
+
+See the full transmission [here](demo_full.png)
+
---
+++ __init__.py
@@ -0,0 +1,9 @@
+# Author: Simon Struck
+# Version: 1.0
+# Date: 25.08.2022
+
+'''
+This decoder stacks on top of the 'i2c' decoder and decodes the ST25DV protocol.
+'''
+
+from .pd import Decoder
